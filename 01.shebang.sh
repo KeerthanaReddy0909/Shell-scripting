f556c8fc3524b4afdeb1ebb614a80771828c2441
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-17 16:02:05 -0400

    Auto push

---
+++ 01.shebang.sh
@@ -0,0 +1,18 @@
+#!bin/bash 
+
+##Start of the line in any bash scrpit is called a shebang notation
+
+##Apart from the firts line none of the lines are considered as shebang notation.
+
+## Shebang nottaion always start with #!
+
+## using shebang notation we are telling system the interpreter to run this.
+
+## now if you run the script , how system will come to know which interpreter  it has to use, that's where SHEBANG notation helps
+
+## how to run a linux script    
+    . bash scriptname.sh
+    .sh scriptname.sh
+    ./scriptname
+
+    echo Welcome to AWS Training
---
+++ Agenda.md
@@ -0,0 +1,14 @@
+This is a repo  to publish all the  Bash basics and project Automation
+
+    # This repo is craeted  to share all the linux basics.
+
+    # Along with linux bash syntax.
+    
+    # Then we will automate the infrastructure provisioning.
+    
+    # And then cong management.
+
+
+    ##### Topics to cover:
+
+    # Shebang Notation and comments

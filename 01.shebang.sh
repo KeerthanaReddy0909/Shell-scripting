3bbee4f5eb1c9f9deb62471f7d99fda614fd877a
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-18 11:56:45 -0400

    Auto push

--- 01.shebang.sh
+++ 01.shebang.sh
@@ -16,5 +16,21 @@
   ## * ./scriptname
 
     echo Welcome to AWS Training
+
+    ## Multiple lines to print 
+
     echo bash is very intresting to learn
     echo make it 2 videos everyday
+
+    ## In bash, we have excape sequence charecters, using that we can some power to the echo. 
+
+    # \n - new line
+    # \t - tab space
+
+    echo -e "line1\nline2" 
+        ##                  line1
+        ##                  line2
+    echo -e "line1\n\tline2"
+        ##                  line1
+        ##                      line2
+        

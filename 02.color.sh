d99aa64d43b6d274705389eeca2b41d8a16726a1
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-18 12:16:18 -0400

    Auto push

---
+++ 02.color.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+# each an every color you see on terminal  will have acolor code  and we need to use that code based on the need
+
+##          Color Foreground Code Background Code
+
+##          Black       30              40
+##          Red         31              41
+##          Green       32              42
+##          Brown       33              43

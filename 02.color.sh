862909f9c97f1fee060fd9c9e4da028c72a45f87
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-18 12:27:41 -0400

    Auto push

--- 02.color.sh
+++ 02.color.sh
@@ -16,3 +16,11 @@
 ##   echo -e "\e[32m I am printing green color \e[0m"
 
 echo -e "\e[32m I am printing green color \e[0m"
+
+echo -e "\e[31m I am printing red color \e[0m"
+echo -e "\e[33m I am printing brown color \e[0m"
+echo -e "\e[34m I am printing yellow color \e[0m"
+
+
+
+

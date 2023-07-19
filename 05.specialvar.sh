798d36138a0294798f44a02a30c53aa99ebb49da
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-19 11:50:22 -0400

    Auto push

--- 05.specialvar.sh
+++ 05.specialvar.sh
@@ -4,11 +4,13 @@
 
 # Hare are some special variables
 # $0 -$9, $?,$*,$#,$@.
-echo $0    ## prints the spcipt name
-echo $?
-echo $*
-echo $@
+echo $0 ## prints the script name
+echo $? ## used to print the exit code the last command.
+echo $# ## is going to print the no of Auguments.
+echo $$ ## used to print the PID of the current process.
+echo $* ## used to print the used variables
+echo $@ ## used to print the used variables
 echo "name of the script executed is $0"
 echo "name of the trainer is $1"
-echo "Training batch no is $3"
-echo "Current topic is $4" 
+echo "Training batch no is $2"
+echo "Current topic is $3" 

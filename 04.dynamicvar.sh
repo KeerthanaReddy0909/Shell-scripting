d64f20d52db31fbe0bc53a35c72e564d9222ded3
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-19 11:08:02 -0400

    Auto push

--- 04.dynamicvar.sh
+++ 04.dynamicvar.sh
@@ -18,5 +18,5 @@ echo -e "good morning and todays date is \e[32m $TODAYS_DATE \e[0m"
 
 ## cat -n /etc/passwd
 
-NO_OF_SESSIONS= $(who | wc -l)
+NO_OF_SESSIONS=$(who | wc -l)
 echo -e "Total no of opened session are \e[32m $NO_OF_SESSIONS \e[0m"

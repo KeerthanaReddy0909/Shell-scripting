c44e04fe05bf70d3070912af98db50669015a710
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-19 12:11:31 -0400

    Auto push

---
+++ 07.commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# There are 4 types of commands available
+
+# 1. Binary - (/bin,/sbin)type top. etc
+
+# 2. Aliases - (Alias are shortcuts, alias net ="netstat-tulpn")
+# 3. Shell built-in commands ()
+# 4. Functions - Functions are nothing but a set of commands that are written i sequence  and can call no of times as per req.
---
+++ 08.function.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# what is fucntion?
+# Function is to capture the repitative block and can be  used on need basics.
+
+# When to use Function?
+# Whenever if you want to same block of code n number of times, rather than writting it multiple times, declare the code as function and call multiple times.
+
+# Deckaring a function
+sample () {
+    echo "this is my firts sample functiom"
+    echo "I am executing a sample functiom"
+    echo "successfully executed the function"
+    echo "yahhh"
+}

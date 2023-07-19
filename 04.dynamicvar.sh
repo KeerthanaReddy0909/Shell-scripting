c3d9a8f93e3b04fa8a57b1ac32e3183ea8338817
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-19 10:40:46 -0400

    Auto push

--- 04.dynamicvar.sh
+++ 04.dynamicvar.sh
@@ -2,6 +2,10 @@
 
 echo "good morning and today's date is 18july2023"
 
-TODAY_DATE = '19july2023'
+##TODAY_DATE = "19july2023"   ## Hard coding the value
 
-echo "good morning and todays date is "TODAY_DATE"
+## echo "good morning and todays date is "TODAY_DATE"
+
+TODAYS_DATE=$(date +%d)  ## always the expression is enclosed in (). put $ so the value of the expression is stored in TODAYS_DATE.
+
+echo good morning and todays date is "$TODAYS_DATE"

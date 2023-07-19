0bd22eb679cd950ff767a8619a0508b631ce019e
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-18 23:12:31 -0400

    Auto push

--- 03.variable.sh
+++ 03.variable.sh
@@ -32,7 +32,7 @@ echo value of the variable d is $d
 ## to print in color
 
 echo $a
-echo -e "value of the variable a \e[32m $a \e0m "
-echo -e "valueof the variable b=$b\nvalue of the variable c= \e[34m; $c \e0m "
-echo -e "value of the variable b is \e[36m; $b \e0m "
-echo -e "value of the variable d is \e[37m; $d \e0m "
+echo -e "value of the variable a \e[32m $a \e[0m "
+echo -e "valueof the variable b=$b\nvalue of the variable c= \e[34m; $c \e[0m "
+echo -e "value of the variable b is \e[36m; $b \e[0m "
+echo -e "value of the variable d is \e[37m; $d \e[0m "

b89f99ee89d6cc29c9cc1a2a69c94eaeb7ede75d
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-19 11:39:31 -0400

    Auto push

--- 05.specialvar.sh
+++ 05.specialvar.sh
@@ -8,4 +8,7 @@ echo $0    ## prints the spcipt name
 echo $?
 echo $*
 echo $@
-echo $1 
+echo "name of the script executed is $0"
+echo "name of the trainer is $1"
+echo "Training batch no is $3"
+echo "Current topic is $4" 

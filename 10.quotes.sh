a9a204655b5a6808eb30d92c134ea73cf5917534
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-25 12:48:21 -0400

    Auto push

---
+++ 10.quotes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# in bash output of a commands will be completely changes based on the quotes used
+# " " - double quotes
+# '' - single quotes
+
+a =53
+echo " value of variable a ${a}"
+echo value of variable a ${a}
+echo 'value of variable a ${a}'

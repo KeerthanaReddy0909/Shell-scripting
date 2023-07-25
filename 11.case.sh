c276fc6fee173e6eec11db91a27f3a961d9a99e8
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-25 12:55:25 -0400

    Auto push

---
+++ 11.case.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# syntax of case
+# case $var in
+#    opt1) command;;
+#   opt2) command ;;
+
+#esac
+
+ACTION=1
+case $ACTION in
+    start)
+        echo " starting RabbitMQ service"
+        ;;
+    stop)
+        echo " stopping RabbitMQ service"
+        ;;
+    restart)
+        echo " Restarting RabbitMQ service"
+        ;;
+    *)
+        echo " Possible values are start or stop value only"
+        ;;
+esac

a22783d844984f0b68e197251a967cd6a20e05e6
Keerthana <keerthana.ravula0909@gmail.com>
2023-07-26 13:31:38 -0400

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,7 +21,7 @@ fi
 }
 #source components/common.sh
 
-#echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
 echo -n "Installing Nginx :"
 yum install nginx -y  &>> $LOGFILE
